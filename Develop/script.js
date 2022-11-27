6560e32a2efa058b7ac0da897b5a6957a7789abb
BenjiCCB <bensdecker+CCB@gmail.com>
2022-11-27 17:26:10 -0500

    pw generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,31 @@
 // Assignment code here
-var charLength = window.prompt("Please enter a desired character length between 8 and 128 characters")
-var charSet = window.prompt("Please enter desired character sets, including lowercase(L), uppercase(U), numeric(N), and special(S)")
 
 function generatePassword(){
-  // validate charLength
-  if (typeOf charLength != 'number' || charLength <= 8 || charLength >= 128){
-    window.alert("Invalid length input.  Please refresh the page and try again")  
+  var charLength = window.prompt("Please enter a desired character length between 8 and 128 characters")
+  var charLengthValidated;
+
+  // validate charLength -- how do I do TypeOf to validate numeric?
+  if (charLength <= 8 || charLength >= 128){
+    window.alert("Invalid length input.  Please try again")  
   } else {
-    var charLengthValidated = charLength;
+    charLengthValidated = true;
   }
+
   // validate charSet
+
+  if (charLengthValidated){
+    var charSet = window.prompt("Please enter desired character sets, including lowercase(L), uppercase(U), numeric(N), and special(S)")
+  }
+  var charSetValidated = true;
+  var typeLower = false;
+  var typeUpper = false;
+  var typeNumeric = false;
+  var typeSpecial = false;
+
+
   for (var i = 0; i < charSet.length; i++) { 
-    if (charSet[i] != "L" || charSet[i] != "U" || charSet[i] != "N" || charSet[i] != "S"){
-      window.alert("Invalid type input. Please refresh the page and try again")  
+    if (charSet[i] != "L" && charSet[i] != "U" && charSet[i] != "N" && charSet[i] != "S"){ // add commas, and spaces?  ALSO TO LOWER
+      charSetValidated = false;
     } else if (charSet[i] == "L"){
       var typeLower = true;
     } else if (charSet[i] == "U"){
@@ -23,8 +36,25 @@ function generatePassword(){
       var typeSpecial = true;
     }
   }
+  if(charSetValidated == false){
+    window.alert("Invalid type input. Please try again")  
+  } else{
+      window.alert("Thanks for your input. See password below.")  
+    // create password
+
+    // Special… 33-47 (+58-64)
+    // Numeric… 48-57
+    // Upper… 65-90
+    // Lower… 97-122
+  }
+    return "wahoo"
+}
+
+  // console.log(String.fromCharCode(189, 43, 190, 61));
+  // https://www.ascii-code.com/
 
 
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
