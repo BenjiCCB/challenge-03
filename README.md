19cd22a373d61ae628069cf48edc25823245de49
BenjiCCB <bensdecker+CCB@gmail.com>
2022-11-28 13:13:52 -0500

    added some further error handling

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# challenge-03
-Challenge 03 repo
+# Challenge-03
+## Challenge 03 Repo - Dynamic Password Generator
+
+--
+
+## Directions
+Click "Generate Password" to dynamically generate a password to chosen specifications.  First enter a desired character length between 8 and 128, then enter desired character types, including lowercase(L), uppercase(U), numeric(N), and special(S).  A unique password will then be generated for you
+
+Be careful not to share your password with untrusted sources!
--- script.js
+++ script.js
@@ -5,77 +5,76 @@ function generatePassword(){
   var charLengthValidated;
 
   // validate charLength -- need to add TypeOf to validate numeric...
-  if (charLength <= 8 || charLength >= 128){
-    window.alert("Invalid length input.  Please try again")  
-  } else {
+  if (charLength >= 8 && charLength <= 128){
     charLengthValidated = true;
+  } else {
+    window.alert("Invalid length input.  Please try again")  
+    return "";
   }
 
   // validate charSet
   if (charLengthValidated){
     var charSet = window.prompt("Please enter desired character sets, including lowercase(L), uppercase(U), numeric(N), and special(S)")
+    charSet = charSet.toLowerCase();
+  
+    var charSetValidated;
+    var charSetArray = [];
+    var typeLower = false;
+    var typeUpper = false;
+    var typeNumeric = false;
+    var typeSpecial = false;
+
+    for (var i = 0; i < charSet.length; i++) { 
+      if (charSet[i] != "l" && charSet[i] != "u" && charSet[i] != "n" && charSet[i] != "s" && charSet[i] != "," && charSet[i] != " "){
+        charSetValidated = false;
+      } else if (charSet[i] == "l"){
+        var typeLower = true;
+      } else if (charSet[i] == "u"){
+        var typeUpper = true;
+      } else if (charSet[i] == "n"){
+        var typeNumeric = true;
+      } else if (charSet[i] == "s"){
+        var typeSpecial = true;
+      }
+    }
+    if(charSetValidated == false){
+      window.alert("Invalid type input. Please try again, using only 'L', 'U', 'N', and/or 'S'")  
+    } else{
+        window.alert("Thanks for your input. See password below.")  
+      // create password
+
+        // // create available charcter array using ASCII codes... https://www.ascii-code.com
+      if (typeLower){
+        for (var i = 97; i <= 122; i++) { 
+          charSetArray.push(String.fromCharCode(i));
+        }
+      }
+      if (typeUpper){
+        for (var i = 65; i <= 90; i++) { 
+          charSetArray.push(String.fromCharCode(i));
+        }
+      }
+      if (typeNumeric){
+        for (var i = 48; i <= 57; i++) { 
+          charSetArray.push(String.fromCharCode(i));
+        }
+      }
+      if (typeSpecial){ 
+        for (var i = 33; i <= 47; i++) { 
+          charSetArray.push(String.fromCharCode(i));
+        }
+      }
+      
+        // // create password from array
+      var finalPW = [];
+      for (var i = 0; i < charLength; i++) { 
+        var arrayIndex = Math.floor(Math.random() * charSetArray.length)
+        finalPW.push(charSetArray[arrayIndex]);
+      }
+      finalPW = finalPW.join('')
+    }
+  return finalPW;
   }
-  var charSetValidated;
-  var charSetArray = [];
-  var typeLower = false;
-  var typeUpper = false;
-  var typeNumeric = false;
-  var typeSpecial = false;
-
-
-  for (var i = 0; i < charSet.length; i++) { 
-    if (charSet[i] != "L" && charSet[i] != "U" && charSet[i] != "N" && charSet[i] != "S"){ // add commas, and spaces?  ALSO TO LOWER prob
-      charSetValidated = false;
-    } else if (charSet[i] == "L"){
-      var typeLower = true;
-    } else if (charSet[i] == "U"){
-      var typeUpper = true;
-    } else if (charSet[i] == "N"){
-      var typeNumeric = true;
-    } else if (charSet[i] == "S"){
-      var typeSpecial = true;
-    }
-  }
-  if(charSetValidated == false){
-    window.alert("Invalid type input. Please try again, using only 'L', 'U', 'N', and/or 'S'")  
-  } else{
-      window.alert("Thanks for your input. See password below.")  
-    // create password
-
-    // // create available charcter array using ASCII codes... https://www.ascii-code.com
-    if (typeLower){
-      for (var i = 97; i <= 122; i++) { 
-        charSetArray.push(String.fromCharCode(i));
-      }
-    }
-    if (typeUpper){
-      for (var i = 65; i <= 90; i++) { 
-        charSetArray.push(String.fromCharCode(i));
-      }
-    }
-    if (typeNumeric){
-      for (var i = 48; i <= 57; i++) { 
-        charSetArray.push(String.fromCharCode(i));
-      }
-    }
-    if (typeSpecial){ // add second set
-      for (var i = 33; i <= 47; i++) { 
-        charSetArray.push(String.fromCharCode(i));
-      }
-    }
-    
-    console.log(charSetArray);
-
-
-    // // create password from array
-    var finalPW = [];
-    for (var i = 0; i < charLength; i++) { 
-      var arrayIndex = Math.floor(Math.random() * charSetArray.length)
-      finalPW.push(charSetArray[arrayIndex]);
-    }
-    finalPW = finalPW.join('')
-  }
-    return finalPW;
 }
 
   
